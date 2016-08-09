eb8464ef02106e7d173fa458d696498f2704256c
Jamie Johnson <thegamebabe@gmail.com>
2016-08-08 20:19:42 -0700

    Added Map Header and UML

---
+++ Projects/Visual Studio 2015/Map.h
@@ -0,0 +1,19 @@
+// Map header  
+// Two Dimensional Vector of Boards
+// Encompasses entire "play area" for robots 
+
+#ifndef MAP_H
+#define MAP_H
+
+#include <vector>
+#include <string>
+//#include "Board.h"
+
+
+class Header {
+	//std::vector<std::vector<Board>> map;
+
+	//Header(std::string filename);
+};
+
+#endif // MAP_F
