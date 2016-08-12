97aff3e12f95611f31dbf18cbaec2549e9f9925c
Plot2Code <jordan.cox@gmail.com>
2016-08-12 02:58:14 -0700

    UMLs added working on Tiles
    project still compiles!

---
+++ Tile.cpp
@@ -0,0 +1,50 @@
+//
+//
+
+#include "Tile.h";
+
+
+void loadTile(std::string tileInformation, sf::Vector2f pos, int boardOrientation) {
+
+	std::string tileName;
+
+	std::stringstream sstile;
+
+	sstile << tileInformation;
+
+	std::getline(sstile, tileName, ' ');
+
+	//if (tileName == "floor") {
+
+	//}
+	//else if (tileName == "pit") {
+
+	//}
+	//else if (tileName == "repair") {
+
+	//}
+	//else if (tileName == "gear") {
+
+	//}
+	//else if (tileName == "conv") {
+
+	//}
+	//else if (tileName == "convbr") {
+
+	//}
+	//else if (tileName == "convbl") {
+
+	//}
+	//else if (tileName == "convhr") {
+
+	//}
+	//else if (tileName == "convhl") {
+
+	//}
+	//else if (tileName == "convt") {
+
+	//}
+
+
+
+};
--- Tile.h
+++ Tile.h
@@ -9,16 +9,30 @@
 #include <vector>
 #include "TileFeature.h"
 
+
+
 class Tile : public Object {
 
 	std::vector<TileFeature> features;
+	std::string tileInformation, spriteSheetFilename;
+	sf::Vector2f pos;
+	sf::Vector2f sPos;
+	int boardOrientation;
 
 public:
-	Tile(std::string tileInformation, std::string spriteSheetFilename, sf::Vector2f pos, int orientation /*, sf::Vector2f sPos, sf::Vector2f sSize = TILE_SOURCE_SIZE*/)    //commented out because i will find sPos through parsing etc
+	Tile(std::string tileInformation, std::string spriteSheetFilename, sf::Vector2f pos, int boardOrientation /*, sf::Vector2f sPos, sf::Vector2f sSize = TILE_SOURCE_SIZE*/)    //commented out because i will find sPos through parsing etc
 		: Object(spriteSheetFilename, pos, sf::Vector2i(0, 0), TILE_SOURCE_SIZE) {
-
+		this->tileInformation = tileInformation;
+		this->spriteSheetFilename = spriteSheetFilename;
+		this->pos = pos;
+		this->boardOrientation = boardOrientation;
+		//loadTile(tileInformation, pos, boardOrientation);
 	};   //sf::Vector2f(0,0) is passing a (0,0) to the object constructor, needs to be replaced with sPos (source position)
-	/*void drawTile(sf::RenderWindow &window) { }*/
+	/*void drawTile(sf::RenderWindow &window) { }*/  //remember tile is an Object and Object already has a draw
+
+	void loadTile(std::string tileInformation, sf::Vector2f pos, int boardOrientation);
+	void unloadTile() {};
+
 };
 
 #endif // TILE_H
